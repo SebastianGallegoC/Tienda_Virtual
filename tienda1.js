2a8e0a972e13e22fd4634fa498d9c8d372018144
Jsebas1 <gallegocarrilloj92@gmail.com>
2023-06-08 19:21:24 -0500

    Push 7
    Ahora el carrito es interactivo,

--- tienda1.js
+++ tienda1.js
@@ -1,1 +1,5 @@
-a
+const btnCart = document.querySelector(".contenedor-imagen") //Se llama la clase contenedor-imagen y se pasa a llama  btnCart//
+const contenedorCarritoProductos = document.querySelector(".contenedor-carrito-productos") 
+btnCart.addEventListener("click", () => { //Cada vez que se haga click en el carrito//
+    contenedorCarritoProductos.classList.toggle("hidden-cart") //Va a abrir el contenedor cada vez que haga click
+})
